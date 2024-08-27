8ac9bda915739f133c07c471c3805458dd227ca0
Peteris <blankfeldspeteris@gmail.com>
2024-08-27 16:57:49 +0300

    Configured .env for deployment variables

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,31 +5,38 @@ import './index.css';
 import axios from 'axios';
 import reportWebVitals from './reportWebVitals';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
-axios.defaults.withCredentials = true;
 
-// Set up axios interceptor to include token in all requests
-axios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+const setupAxios = async () => {
+  const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://127.0.0.1:8000';
+  axios.defaults.baseURL = backendUrl;
+  axios.defaults.withCredentials = true;
+};
+
+
+setupAxios().then(() => {
+  // Set up axios interceptor to include token in all requests
+  axios.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+  );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    < Routing/>
-  </React.StrictMode>
-);
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <Routing />
+    </React.StrictMode>
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+});
